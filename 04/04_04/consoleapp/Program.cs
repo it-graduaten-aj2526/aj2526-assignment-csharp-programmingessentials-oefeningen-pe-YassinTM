9282aa194dede8c65fdaa216dbf1247427a4c431
YassinTM <r1114367@student.thomasmore.be>
2025-10-06 21:38:20 +0200

    Oefening 0404

--- 04/04_04/consoleapp/Program.cs
+++ 04/04_04/consoleapp/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("symbool 1: ");
+string symbool1 = Console.ReadLine();
+
+System.Console.WriteLine("symbool 2: ");
+string symbool2 = Console.ReadLine();
+
+System.Console.WriteLine("Lengte : ");
+int lengte = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Breedte : ");
+int breedte = int.Parse(Console.ReadLine());
+
+
+
+
+for (int i = 0; i < lengte; i++)
+{
+    if (i % 2 == 0)
+    {
+        for (int a = 0; a < breedte; a++)
+        {
+            System.Console.Write(symbool1);
+        }
+        System.Console.WriteLine();
+    }
+    else
+    {
+        for (int a = 0; a < breedte; a++)
+        {
+            System.Console.Write(symbool2);
+        }
+        System.Console.WriteLine();
+
+    }
+}
