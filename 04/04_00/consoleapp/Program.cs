79ed1ab6c8db0d3c04b405acf425f08491789b6b
YassinTM <r1114367@student.thomasmore.be>
2025-10-06 19:42:43 +0200

    Commit 4_00

--- 04/04_00/consoleapp/Program.cs
+++ 04/04_00/consoleapp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// bij een for loop weet je ALTIJD hoeveel keer je wilt herhalen/loopen
+//for loop: bestaat uit 3 delen
+//Deel1 = variable aanmaken
+//Deel2 = BOOLEAANSE VOORWAARDEN > IF TRUE = CODE UITVOEREN IN DE LOOP
+//DEEL 3 = WAT DOEN NA DE CODE IS UITGEVOERD
+// DIT LOOPT TOT DAT DEEL 2 = ONWAAR
+int getal;
+Console.Write("Geef een getal in: ");
+getal = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < 10; i++)
+{
+    ++getal;
+    Console.WriteLine($"{getal}");
+}
