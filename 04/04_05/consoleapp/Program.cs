763b697d30ddac5e97175ea9b04b96ee1841d065
YassinTM <r1114367@student.thomasmore.be>
2025-10-06 21:44:59 +0200

    oefening 0405

--- 04/04_05/consoleapp/Program.cs
+++ 04/04_05/consoleapp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("getal1");
+int getal1 = int.Parse(Console.ReadLine()); 
+
+System.Console.WriteLine("getal2");
+int getal2 = int.Parse(Console.ReadLine());
+
+for (int i = getal1; i >= getal2; i--)
+{
+    System.Console.Write(i);
+    if (i > getal2)
+    {
+        System.Console.Write(" * ");
+    }
+}
