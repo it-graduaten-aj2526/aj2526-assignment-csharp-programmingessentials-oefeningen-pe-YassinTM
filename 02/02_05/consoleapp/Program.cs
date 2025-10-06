2c4a81081a6c6bcf264d0862d8a77ced08a4388c
YassinTM <r1114367@student.thomasmore.be>
2025-10-06 22:01:46 +0200

    fixed  0205

--- 02/02_05/consoleapp/Program.cs
+++ 02/02_05/consoleapp/Program.cs
@@ -5,19 +5,19 @@ landingsplaats = int.Parse(Console.ReadLine());
 
 if (landingsplaats == 1)
 {
-    Console.WriteLine("0 punten");
+    Console.WriteLine("0 punten.");
 }
 else if (landingsplaats == 2)
 {
-Console.WriteLine("20 punten");
+Console.WriteLine("20 punten.");
 }
 else if (landingsplaats == 3)
 {
-Console.WriteLine("50 punten");
+Console.WriteLine("50 punten.");
 }
 else if (landingsplaats == 4)
 {
-Console.WriteLine("100 punten");
+Console.WriteLine("100 punten.");
 }
 else
 { 
