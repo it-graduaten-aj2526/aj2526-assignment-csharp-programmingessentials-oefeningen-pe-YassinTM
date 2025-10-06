1b00132ac2d49e4a2f8579556b83aa75a179332a
YassinTM <r1114367@student.thomasmore.be>
2025-10-06 20:12:13 +0200

    Oefening 0402

--- 04/04_02/consoleapp/Program.cs
+++ 04/04_02/consoleapp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Naam: ");
+string naam = Console.ReadLine();
+
+System.Console.WriteLine("Geluksgetal: ");
+int geluksgetal = int.Parse(Console.ReadLine());
+
+System.Console.Write(geluksgetal + " ");
+for (int i = 0; i < geluksgetal; i++)
+{
+System.Console.Write(naam + " ");
+}
