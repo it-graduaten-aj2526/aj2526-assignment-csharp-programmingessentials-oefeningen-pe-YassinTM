4713cf7a2adf0c02e3b431bbcf122c898ef1f619
YassinTM <r1114367@student.thomasmore.be>
2025-10-08 21:50:53 +0200

    oefening 0305

--- 03/03_05/consoleapp/Program.cs
+++ 03/03_05/consoleapp/Program.cs
@@ -1,3 +1,26 @@
-﻿//version 2
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Coordinaat 1");
+int coordinaat1 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Coordinaat 2");
+int coordinaat2 = int.Parse(Console.ReadLine());
+
+if (coordinaat1 == 0 && coordinaat2 == 0)
+{
+    System.Console.WriteLine("Punt ligt in de oorsprong.");
+}
+else if (coordinaat1 >= 0 && coordinaat2 >= 0)
+{
+    System.Console.WriteLine("Punt ligt in het eerste kwadrant.");
+}
+else if (coordinaat1 < 0 && coordinaat2 >= 0)
+{
+    System.Console.WriteLine("Punt ligt in het tweede kwadrant.");
+}
+else if (coordinaat1 < 0 && coordinaat2 < 0)
+{
+    System.Console.WriteLine("Punt ligt in het derde kwadrant.");
+}
+else
+{
+    System.Console.WriteLine("Punt ligt in het vierde kwadrant.");
+}
