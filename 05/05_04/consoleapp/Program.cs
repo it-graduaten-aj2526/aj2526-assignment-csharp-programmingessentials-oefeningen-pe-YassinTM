5902e5e4eef20bf10b5a0de00b4f8112dbbf0adb
YassinTM <r1114367@student.thomasmore.be>
2025-10-18 21:26:41 +0200

    0504 finish

--- 05/05_04/consoleapp/Program.cs
+++ 05/05_04/consoleapp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string input, zin = "Op de stoel ligt een zonnehoed en peperkoek";
+int attempt = 0;
+bool conditioning = true;
+
+System.Console.WriteLine("Op de stoel ligt een zonnehoed en peperkoek");
+do
+{
+    input = Console.ReadLine();
+    attempt++;
+    if (input == zin)
+    {
+        conditioning = false;
+    }
+
+} while (conditioning);
+
+if (attempt == 1)
+{
+    System.Console.WriteLine($"Je had {attempt} poging nodig!");
+}
+else
+{
+    System.Console.WriteLine($"Je had {attempt} pogingen nodig!");
+}
