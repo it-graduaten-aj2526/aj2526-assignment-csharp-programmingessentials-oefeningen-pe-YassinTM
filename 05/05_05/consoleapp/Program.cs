9c190952e8b4c02674977aa7d146f570aa923b55
YassinTM <r1114367@student.thomasmore.be>
2025-10-18 21:39:38 +0200

    0505 finished

--- 05/05_05/consoleapp/Program.cs
+++ 05/05_05/consoleapp/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int input, punten = 0;
+bool notvalidnumber;
+
+for (int i = 0; i < 3; i++)
+{
+
+
+    do
+    {
+        System.Console.Write("geef een nummer in: ");
+        int.TryParse(Console.ReadLine(), out input);
+        switch (input)
+        {
+            case 1:
+                notvalidnumber = false;
+                punten = punten + 0;
+                break;
+            case 2:
+                notvalidnumber = false;
+                punten = punten + 20;
+                break;
+            case 3:
+                notvalidnumber = false;
+                punten = punten + 50;
+                break;
+            case 4:
+                notvalidnumber = false;
+                punten = punten + 100;
+
+                break;
+            default:
+                notvalidnumber = true;
+                break;
+        }
+
+
+    } while (notvalidnumber);
+}
+System.Console.WriteLine($"U hebt {punten} punten behaald");
