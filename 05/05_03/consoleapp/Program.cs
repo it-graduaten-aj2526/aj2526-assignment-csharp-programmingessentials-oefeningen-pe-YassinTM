63377b91e0a5b11e5e4177c865e4048016466395
YassinTM <r1114367@student.thomasmore.be>
2025-10-18 21:14:10 +0200

    finish 0503

--- 05/05_03/consoleapp/Program.cs
+++ 05/05_03/consoleapp/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int y = 0, n = 0, passagiers = 0;
+bool trueorfalse = true;
+do
+{
+    System.Console.WriteLine("Y of N: ");
+    string input = Console.ReadLine();
+
+    switch (input.ToUpper())
+    {
+        case "N":
+            n++;
+            passagiers++;
+
+            break;
+
+        case "Y":
+            y++;
+            passagiers++;
+
+            break;
+    }
+if (y == 4 || passagiers == 8)
+    {
+        trueorfalse = false;
+    }
+
+} while (trueorfalse);
+
+System.Console.WriteLine($"Er zijn {passagiers} passagiers. {y} zitten er aan het raam.");
