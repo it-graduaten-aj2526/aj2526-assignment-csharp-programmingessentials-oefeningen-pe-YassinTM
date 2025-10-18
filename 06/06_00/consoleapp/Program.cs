12848b4fd8c1f9e67eabb401ab83efc8577bc90f
YassinTM <r1114367@student.thomasmore.be>
2025-10-19 00:50:16 +0200

    try 0602

--- 06/06_00/consoleapp/Program.cs
+++ 06/06_00/consoleapp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int getal;
+int som = 0;
+bool istrue;
+
+do
+{
+    System.Console.WriteLine("Geef een getal: ");
+    istrue = int.TryParse(Console.ReadLine(), out getal);
+} while (!istrue);
+
+while (getal > 0)
+{
+    som += getal;
+    do
+{
+    System.Console.WriteLine("Geef een getal: ");
+    istrue = int.TryParse(Console.ReadLine(), out getal);
+} while (!istrue);
+}
+System.Console.WriteLine($"Totaal: {som}");
--- 06/06_02/consoleapp/Program.cs
+++ 06/06_02/consoleapp/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string voornaam, achternaam;
+int hobbynummer;
+bool numberornot, conditional = true;
+{
+    System.Console.WriteLine("voornaam: ");
+    voornaam = Console.ReadLine();
+
+} while (string.IsNullOrEmpty(voornaam));
+
+do
+{
+    System.Console.WriteLine("achternaam: ");
+    achternaam = Console.ReadLine();
+
+} while (string.IsNullOrEmpty(achternaam));
+
+do
+{
+
+    do
+    {
+
+        System.Console.WriteLine("hobby nummer");
+        numberornot = int.TryParse(Console.ReadLine(), out hobbynummer);
+
+    } while (!numberornot);
+
+    
+    switch (hobbynummer)
+        {
+            case 1:
+            System.Console.WriteLine("Wij raden \"Anna\" aan.");
+            conditional = true;
+            break;
+
+            case 2:
+            System.Console.WriteLine("Wij raden \"Knippie\" aan.");
+conditional = true;
+            break;
+
+            case 3:
+            System.Console.WriteLine("Wij raden \"VtWonen\" aan.");
+conditional = true;
+            break;
+
+            case 4:
+            System.Console.WriteLine("Wij raden \"Voetbal International\" aan.");
+conditional = true;
+            break;
+
+            case 5:
+            System.Console.WriteLine("Wij raden \"Wandelen & fietsen\" aan.");
+conditional = true;
+            break;
+
+            case 6:
+            System.Console.WriteLine("Wij raden \"Zoom NL\" aan.");
+conditional = true;
+            break;
+
+            case 7:
+
+            System.Console.WriteLine("Wij raden \"Runners\" aan.");
+            conditional = true;
+            break;
+
+            case 8:
+            System.Console.WriteLine("Wij raden niets aan.");
+            conditional = false;
+            break;
+        }
+
+} while (conditional);
