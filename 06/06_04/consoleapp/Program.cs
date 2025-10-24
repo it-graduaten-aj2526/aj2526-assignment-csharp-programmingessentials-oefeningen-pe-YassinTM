5007353f2e8ab33150f241bc9c3ca1c5d605dc62
YassinTM <r1114367@student.thomasmore.be>
2025-10-24 20:48:39 +0200

    0604 finish

--- 06/06_04/consoleapp/Program.cs
+++ 06/06_04/consoleapp/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int tafel;
+bool tafelnum;
+do
+{
+
+    System.Console.WriteLine("geef een tafel in");
+    tafelnum = int.TryParse(Console.ReadLine(), out tafel);
+} while (!tafelnum);
+
+while(tafel != 0)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        System.Console.WriteLine($"{i + 1} x {tafel} = {(i + 1) * tafel} ");
+    }
+
+    do
+{
+
+    System.Console.WriteLine("geef een tafel in");
+    tafelnum = int.TryParse(Console.ReadLine(), out tafel);
+} while (!tafelnum);
+}
