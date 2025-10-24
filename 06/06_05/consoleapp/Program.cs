efebc1433ab6e05bb696c465d6261b3c57685b7e
YassinTM <r1114367@student.thomasmore.be>
2025-10-24 21:38:53 +0200

    0605 finish

--- 06/06_05/consoleapp/Program.cs
+++ 06/06_05/consoleapp/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string naam, overzicht = "";
+int score, totaalscore = 0, aantal = 0;
+bool isscorenum, endloop = true;
+
+
+
+System.Console.WriteLine("Naam?");
+naam = Console.ReadLine();
+
+if (string.IsNullOrEmpty(naam))
+{
+    System.Console.WriteLine("Geen aanwezigen");
+
+}
+else
+{
+    do
+    {
+        System.Console.WriteLine("cijfer");
+        isscorenum = int.TryParse(Console.ReadLine(), out score);
+    } while (!isscorenum);
+    overzicht = overzicht + naam + $"({score})\n";
+    totaalscore = totaalscore + score;
+    aantal++;
+
+    do
+    {
+        System.Console.WriteLine("Naam?");
+        naam = Console.ReadLine();
+        if (string.IsNullOrEmpty(naam))
+        {
+            System.Console.WriteLine(overzicht);
+            endloop = false;
+        }
+        else
+        {
+            do
+            {
+                System.Console.WriteLine("cijfer");
+                isscorenum = int.TryParse(Console.ReadLine(), out score);
+            } while (!isscorenum);
+            overzicht = overzicht + naam + $"({score})\n";
+            totaalscore = totaalscore + score;
+                    aantal++;
+
+
+
+            endloop = true;
+        }
+
+
+    } while (endloop);
+    System.Console.WriteLine($"Gemiddelde: {totaalscore/aantal}");
+
+
+
+
+}
+
