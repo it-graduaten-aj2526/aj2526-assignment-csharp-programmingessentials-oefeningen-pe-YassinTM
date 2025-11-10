785db6a8a2a3d0b93beb9710092c191ba0cffef1
YassinTM <r1114367@student.thomasmore.be>
2025-11-10 22:56:03 +0100

    OEF 0703

--- 07/07_03/consoleapp/Program.cs
+++ 07/07_03/consoleapp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string input;
+int inputcijfer;
+List<int> cijferlijst = [];
+
+
+
+while (true)
+{
+    do
+    {
+        System.Console.WriteLine("Geef een cijfer in: ");
+        input = Console.ReadLine();
+    } while (!int.TryParse(input, out inputcijfer) && !string.IsNullOrEmpty(input));
+
+    if (string.IsNullOrEmpty(input))
+    {
+        break;
+    }
+    else
+    {
+            cijferlijst.Add(inputcijfer);
+
+    }
+}
+
+cijferlijst.Sort();
+System.Console.WriteLine($"Laagste getal: {cijferlijst.First()}\nHoogste getal: {cijferlijst.Last()}");
