4faf4cadf8c1cf9463d6777142d1f3b9b23ec9a5
YassinTM <r1114367@student.thomasmore.be>
2025-11-10 20:10:30 +0100

    OEF 0700

--- 07/07_00/consoleapp/Program.cs
+++ 07/07_00/consoleapp/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//array syntax: int[] numbers = [0, 1, 2, 3]; console.writeline(numbers[0])
+//foreach is for but to list out items in a collection like array/list
+//array = vaste waarde, List = length is adjustable
+//list syntax List<int> numberList = [0, 1, 2, 3]; 
+//omdat list dynamically is kan je numberList.Add(waarde) en numberList.Remove(waarde)
+string antwoord, naam;
+List<string> namenlijst = [];
+do
+{
+    System.Console.WriteLine("Wil je een naam toevoegen?");
+    antwoord = Console.ReadLine();
+} while (antwoord.ToLower() != "ja" && antwoord.ToLower() != "nee");
+
+while (antwoord.ToLower() == "ja")
+{
+
+    do
+    {
+        System.Console.Write("Geef een naam in: ");
+        naam = Console.ReadLine();
+    } while (string.IsNullOrEmpty(naam));
+    namenlijst.Add(naam);
+
+    do
+    {
+        System.Console.WriteLine("Wil je een anam toevoegen?");
+        antwoord = Console.ReadLine();
+    } while (antwoord.ToLower() != "ja" && antwoord.ToLower() != "nee");
+
+}
+foreach (string n in namenlijst)
+{
+    System.Console.WriteLine(n);
+}
