d9a73a13814013184a839f44bd2004640a11de93
YassinTM <r1114367@student.thomasmore.be>
2025-11-10 22:19:22 +0100

    OEF 0701

--- 07/07_01/consoleapp/Program.cs
+++ 07/07_01/consoleapp/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int intcijfer, posgetallen = 0, negsom = 0;
+string inputcijfer;
+List<int> cijferlijst = [];
+
+
+   do
+   {
+    System.Console.WriteLine("Geef een cijfer in");
+    inputcijfer = Console.ReadLine();
+
+    if (int.TryParse(inputcijfer, out intcijfer))
+    {
+        cijferlijst.Add(intcijfer);
+    }
+    
+   } while (!string.IsNullOrEmpty(inputcijfer));
+
+
+
+
+
+
+foreach (int item in cijferlijst)
+{
+    if (item > 0)
+    {
+        posgetallen++;
+    }
+    else if (item < 0)
+    {
+        negsom += item;
+    }
+}
+System.Console.WriteLine($"Aantal positief: {posgetallen}\nNegatieve som: {negsom}");
