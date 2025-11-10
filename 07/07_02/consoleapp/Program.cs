1668a4a5a0f112f7e95d086ea7cc6451a0d2a42a
YassinTM <r1114367@student.thomasmore.be>
2025-11-10 22:38:14 +0100

    OEF 0702

--- 07/07_02/consoleapp/Program.cs
+++ 07/07_02/consoleapp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> cijferlijst = [];
+string antwoord;
+
+int inputcijfer = 0;
+
+while (true)
+{
+    System.Console.WriteLine("Wil je een cijfer toevoegen aan de list?");
+    antwoord = Console.ReadLine();
+
+    if (antwoord.ToLower() == "ja")
+    {
+        do
+        {
+            System.Console.Write("Geef een cijfer in: ");
+        } while (!int.TryParse(Console.ReadLine(), out inputcijfer));
+        cijferlijst.Add(inputcijfer);
+    }
+    else if (antwoord.ToLower() == "nee")
+    {
+        break;
+    }
+
+}
+cijferlijst.Sort();
+
+System.Console.WriteLine($"Laagste cijfer: {cijferlijst.First()}\nHoogste cijfer: {cijferlijst.Last()}\nGemiddelde: {cijferlijst.Average()}");
