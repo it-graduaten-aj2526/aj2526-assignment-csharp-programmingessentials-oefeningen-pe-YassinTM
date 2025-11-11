fdd45b0e49c1464e4ccc82fa6a316101582bd88c
YassinTM <r1114367@student.thomasmore.be>
2025-11-11 12:34:09 +0100

    OEF 0705

--- 07/07_05/consoleapp/Program.cs
+++ 07/07_05/consoleapp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int inputgetal;
+List<int> getallenlijst = [];
+do
+{
+    System.Console.WriteLine("Geef een getal in, 0 op te eindigen: ");
+} while (!int.TryParse(Console.ReadLine(), out inputgetal));
+
+while (inputgetal != 0)
+{
+    getallenlijst.Add(inputgetal);
+    do
+    {
+        System.Console.WriteLine("Geef een getal in, 0 op te eindigen: ");
+    } while (!int.TryParse(Console.ReadLine(), out inputgetal));
+
+}
+getallenlijst.Sort();
+
+if (getallenlijst.Count() > 0)
+{
+System.Console.WriteLine("Statistieken:");
+System.Console.WriteLine($"Hoogste cijfer: {getallenlijst.Last()}");
+System.Console.WriteLine($"Laagste cijfer: {getallenlijst.First()}");
+System.Console.WriteLine($"Gemiddelde: {Math.Floor(getallenlijst.Average())}");
+    
+}
+else
+{
+    System.Console.WriteLine("Lege lijst");
+}
