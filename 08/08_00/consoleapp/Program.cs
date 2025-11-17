467e4979cb26f7a6a427d735ae1039ed56b1c9fb
YassinTM <r1114367@student.thomasmore.be>
2025-11-17 20:51:09 +0100

    OEF 0800

--- 08/08_00/consoleapp/Program.cs
+++ 08/08_00/consoleapp/Program.cs
@@ -1,2 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // //Creation of method
+// // //DOES NOT RUN YET 
+
+// // //VOID IS RETURN DATATYPE 
+// // void ToonNaam(string naam, string voornaam)
+// // {
+// //     Console.WriteLine($"{naam}\n{voornaam}");
+// // }
+// // //int is return type
+// // int Optellen(int getal1, int getal2)
+// // {
+// //    //to return something use return 
+// //  return    getal1 + getal2;
+// // }
+
+// // ToonNaam("Moumni", "Yassin");
+// // //Optellen(1,2) geeft 3 maar print niets, geen writeline gebruikt
+// // int som = Optellen(1,2);
+
+// // //of
+// // System.Console.WriteLine(Optellen(1,2));
+// //----
+// string ReadStringNotEmpty()
+// {
+//     string output;
+//     do
+//     {
+//         System.Console.WriteLine("Geef een string in: ");
+//         output = Console.ReadLine();
+
+//     } while (output == "");
+//     return output;
+// }
+// ReadStringNotEmpty();
+
+
+// //MAG NIET
+// //variable=x
+// //methode{
+// //variable++
+// //}
+
+// //GEBRUIK PARAMETERS
+// //variable=x
+// //variable = methode(variable)
+// //methode(PARAMETER){}
+int getal, totaal = 0;
+int ReadNumber()
+{
+    int inputgetal;
+    do
+    {
+        System.Console.Write("Geef een geheel getal: ");
+    } while (!int.TryParse(Console.ReadLine(), out inputgetal));
+    return inputgetal;
+}
+
+bool isNegative(int number)
+{
+    if (number <= 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+do
+{
+getal = ReadNumber();
+if (getal > 0)
+{
+    totaal += getal;
+}
+    
+} while (!isNegative(getal));
+
+
+
+
+System.Console.WriteLine($"Totaal = {totaal}");
